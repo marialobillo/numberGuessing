345e84dd545b5091ceb3f7577d26a3c5d2df03bf
marialobillo <maria.lobillo.santos@gmail.com>
2018-10-10 18:53:53 +0200

    guessing number game

--- guessing_game.py
+++ guessing_game.py
@@ -5,31 +5,52 @@ Project 1 - Number Guessing Game
 
 For this first project we will be using Workspaces.
 
-NOTE: If you strongly prefer to work locally on your own computer, you can totally do that by clicking: File -> Download Workspace in the file menu after you fork the snapshot of this workspace.
+NOTE: If you strongly prefer to work locally on your own computer, you can
+totally do that by clicking: File -> Download Workspace in the file menu after
+you fork the snapshot of this workspace.
 
 """
 
 import random
 
+def show_intro():
+    print("Welcome to Number Guessing Game!")
+    print("""
+Enter 'BYE' to stop the game.
+""")
+
+
+def get_random(a, b):
+    return random.randint(a, b)
+
+
 
 def start_game():
-    """Psuedo-code Hints
+    
+    show_intro()
+    rounds = 0
+    answer = ''
+    random_number = get_random(1, 11)
 
-    When the program starts, we want to:
-    ------------------------------------
-    1. Display an intro/welcome message to the player.
-    2. Store a random number as the answer/solution.
-    3. Continuously prompt the player for a guess.
-      a. If the guess greater than the solution, display to the player "It's lower".
-      b. If the guess is less than the solution, display to the player "It's higher".
+    while answer != 'BYE':
+        answer = input("Please introduce a guess number from 1 to 10 --> ")
 
-    4. Once the guess is correct, stop looping, inform the user they "Got it"
-         and show how many attempts it took them to get the correct number.
-    5. Let the player know the game is ending, or something that indicates the game is over.
+        if answer == 'BYE':
+            break
 
-    ( You can add more features/enhancements if you'd like to. )
-    """
-    # write your code inside this function.
+        user_number = int(answer)
+        rounds += 1
+        if user_number == random_number:
+            print("Got it!")
+            break
+        elif user_number > random_number:
+            print("It's lower")
+        elif user_number < random_number:
+            print("It's higher")
+
+
+    print("You won onlt in {} attemps!".format(rounds))
+
 
 
 if __name__ == '__main__':
